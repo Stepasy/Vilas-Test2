72f079e4a5c240a713e497a7de9231c07757b908
Employee <employee@Mac-mini-Just-5.local>
2018-12-17 19:59:08 +0200

    responsive fixes & padding on elements

--- src/js/modules/slick.js
+++ src/js/modules/slick.js
@@ -2,10 +2,10 @@ $(document).ready(function(){
     $('.slick').slick({
         vertical: true,
         slidesToShow: 1,
-        infinite: true,
         verticalSwiping: true,
-        accessibility: false,
         slidesToScroll: 1,
+        infinite: false,
+        accessibility: false,
         nextArrow: '<i class="fa fa-chevron-right"></i>',
         prevArrow: '<i class="fa fa-chevron-left"></i>',
     });
