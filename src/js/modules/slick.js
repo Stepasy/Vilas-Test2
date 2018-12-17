a31dc496cc36ba49f2e89eb6d86c75e6936806fc
Employee <employee@Mac-mini-Just-5.local>
2018-12-17 17:47:59 +0200

    responive fixes & removing elemants padding

--- src/js/modules/slick.js
+++ src/js/modules/slick.js
@@ -2,12 +2,11 @@ $(document).ready(function(){
     $('.slick').slick({
         vertical: true,
         slidesToShow: 1,
-        slidesToScroll: 1,
-        verticalSwiping: true,
         infinite: true,
+        verticalSwiping: true,
         accessibility: false,
+        slidesToScroll: 1,
         nextArrow: '<i class="fa fa-chevron-right"></i>',
         prevArrow: '<i class="fa fa-chevron-left"></i>',
-        adaptiveHeight: true
     });
 });
