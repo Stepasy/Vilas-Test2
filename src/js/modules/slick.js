eca609fbc5c54755101e1d107a36638820e48d4e
Stepasy <sad100500@gmail.com>
2018-12-18 13:22:30 +0200

    fixed slider, responsive updates

--- src/js/modules/slick.js
+++ src/js/modules/slick.js
@@ -9,4 +9,15 @@ $(document).ready(function(){
         nextArrow: '<i class="fa fa-chevron-right"></i>',
         prevArrow: '<i class="fa fa-chevron-left"></i>',
     });
+    let maxHeight = -1;
+    $('.slick-slide').each(function() {
+        if ($(this).height() > maxHeight) {
+            maxHeight = $(this).height();
+        }      
+    });
+    $('.slick-slide').each(function() {
+        if ($(this).height() < maxHeight) {
+    $(this).css('margin', Math.ceil((maxHeight-$(this).height())/2) + 'px 0');
+    }
+});
 });
